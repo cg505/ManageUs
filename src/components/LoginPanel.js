e3bb88286a0fd37e9eb58474daebcef1dd19d426
Devin Sova <neoskeeter7@gmail.com>
2018-04-10 15:24:20 -0400

    added LoginPanel.js and Components.css

---
+++ src/components/LoginPanel.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Component.css'
+
+class LoginPanel extends Component {
+  render() {
+    //Bootstrap Login Window
+    return (
+      <div className="LoginPanel">
+      </div>
+    )
+  }
+}
