31b492e00abe2c809926a8bffb583a4c1695de0b
unknown <765044145@qq.com>
2018-04-11 22:50:43 -0400

    add login to main page

--- src/components/LoginPanel.js
+++ src/components/LoginPanel.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import './Component.css'
-
-class LoginPanel extends Component {
-  render() {
-    //Bootstrap Login Window
-    return (
-      <div className="LoginPanel">
-      </div>
-    )
-  }
+import './Components.css'
+import {
+    NavLink
+} from "react-router-dom"
+class Login extends Component {
+    render() {
+        //Bootstrap Login Window
+        return (
+            <div className="Login">
+                <h1>Login</h1>
+                <input type="text" placeholder="Username" required="" autoFocus="" />
+                <input type="password" placeholder="Password" required=""/>
+                <br/>
+                <NavLink to="/APP"><input type="submit" /></NavLink>
+            </div>
+        )
+    }
+    handleLoginClick() {
+        console.log("Hello World!")
+    }
 }
+
+export default Login;
