6b667b8a26e9bdb986352bd78c644c8e072f41f8
Devin Sova <neoskeeter7@gmail.com>
2018-04-21 23:11:28 -0400

    Added Basic Modal

--- src/components/ChoresPanel.js
+++ src/components/ChoresPanel.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
 import './Components.css';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root')
 
 class ChoresPanel extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+        modalIsOpen: false
+        };
+
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
     render() {
         return (
-            <a href="/Chores" className="flex-item" id="Chores">
+            <div className="flex-item" id="Chores">
                 <div className = "flex-item-header">
                     <h4>Chores</h4>
                 </div>
+
+                <button onClick={this.openModal}>Open Modal</button>
+                <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
+
                 <span className="badge badge-primary badge-pill">1 day ago</span>
                 <h5 className="mb-1">Chores List</h5>
                 <p className="mb-1">add some thing here</p>
                 <small>by XXX</small>
-            </a>
+            </div>
         );
     }
 }
