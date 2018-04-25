9e7b6509b8a4a4d3b697f7e1f9e24de00147a292
Christopher Cooper <christopher.c.cooper@gmail.com>
2018-04-24 23:18:20 -0400

    add rules model

--- server/models/household.js
+++ server/models/household.js
@@ -18,6 +18,10 @@ module.exports = (sequelize, DataTypes) => {
                 foreignKey: 'householdId'
             })
         );
+
+        Household.hasOne(models.Rules, {
+            foreignKey: 'householdId'
+        });
     };
 
     return Household;
