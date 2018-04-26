6fc9eb99f6ebe0072f1f508630f2d6abf85f91f4
Devin Sova <neoskeeter7@gmail.com>
2018-04-25 22:12:27 -0400

    Styled join and create household

--- src/components/CreateHousehold.js
+++ src/components/CreateHousehold.js
@@ -6,6 +6,7 @@ import {
     Button
 } from 'react-bootstrap';
 import authFetch from '../utils/authFetch';
+import createImage from '../img/create.png';
 
 class CreateHousehold extends Component {
     constructor(props) {
@@ -44,11 +45,12 @@ class CreateHousehold extends Component {
 
     render() {
         return (
-            <div>
+            <div className="flex-item create">
                 {
                     this.state.error &&
                     (<p>{this.state.error}</p>)
                 }
+                <h3><img src={createImage} alt="createImage" /> Create a Household</h3>
                 <Form inline onSubmit={this.submit}>
                     <FormGroup>
                         <FormControl type="text" placeholder="Name" onChange={this.handleChange} value={this.state.name} />
