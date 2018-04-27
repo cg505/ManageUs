87ae832ea11b594c9fe78190809e80ee73c77f05
Mizunarei <765044145@qq.com>
2018-04-27 03:51:12 -0400

    add Nameapp

---
+++ src/components/NameApps.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './Components.css';
+import authFetch from "../utils/authFetch";
+
+class NameApps extends Component {
+    constructor(){
+        super();
+        this.state = {
+            items: [],
+            houseHoldInfo: null,
+        };
+
+        this.fetchUser = this.fetchUser.bind(this);
+    }
+    componentDidMount() {
+        this.fetchUser();
+    }
+
+    async fetchUser() {
+        const resp = await authFetch('/api/households');
+        if(resp.ok) {
+            this.setState({
+                houseHoldInfo: await resp.json()
+            });
+
+            if(this.state.houseHoldInfo) {
+                let temp = this.state.houseHoldInfo.Users;
+                this.setState({
+                    items: temp
+                });
+            }
+        }
+    }
+
+
+    render() {
+        return(
+            <div>
+                {
+                    this.state.items.map(function(key){
+                        return <tr>
+                            <th>
+                                {key.firstName} {key.lastName}
+                            </th>
+                        </tr>
+                    }.bind(this))
+                }
+            </div>
+        )
+    }
+}
+
+export default NameApps;
