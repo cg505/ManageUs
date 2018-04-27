c3b456cd93f664409618bb6058948f1ac692a202
Mizunarei <765044145@qq.com>
2018-04-27 03:50:17 -0400

    finish name panel

--- src/components/GroceryApps.js
+++ src/components/GroceryApps.js
@@ -17,14 +17,14 @@ class GroceryApps extends Component{
 
 
     addItem(item){
-        var time = (new Date()).getTime();
+        const time = (new Date()).getTime();
         this.state.items['item-' + time] = item;
         this.setState({items: this.state.items});
     }
 
     createItems(e){
         e.preventDefault();
-        var item = this.refs.itemName.value;
+        const item = this.refs.itemName.value;
         if(typeof item === 'string' && item.length > 0) {
             this.addItem(item);
             this.refs.itemForm.reset();
@@ -37,29 +37,29 @@ class GroceryApps extends Component{
 
     render() {
         return(
-                <div>
-                    {
-                        Object.keys(this.state.items).map(function(key){
-                            return <tr><th>
-                                {this.state.items[key]}
-                            </th></tr>
-                        }.bind(this))
-                    }
+            <div>
+                {
+                    Object.keys(this.state.items).map(function(key){
+                        return <tr><th>
+                            {this.state.items[key]}
+                        </th></tr>
+                    }.bind(this))
+                }
                 <div className="component-wrapper">
-                <form className="form-inline" ref="itemForm" onSubmit={this.createItems}>
-                    <div className="form-group">
-                        <label htmlFor="Item">
-                            Item Name
-                            <input type="text" id="Item" placeholder="add item name" ref="itemName"
-                                   className="form-control"/>
-                        </label>
+                    <form className="form-inline" ref="itemForm" onSubmit={this.createItems}>
+                        <div className="form-group">
+                            <label htmlFor="Item">
+                                Item Name
+                                <input type="text" id="Item" placeholder="add item name" ref="itemName"
+                                       className="form-control"/>
+                            </label>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Add</button>
+                    </form>
+                    <div className="edit-button" >
+                        <Button bsStyle="info" onClick={this.save}>Save</Button>
                     </div>
-                    <button type="submit" className="btn btn-primary">Add</button>
-                </form>
-                <div className="edit-button" >
-                    <Button bsStyle="info" onClick={this.save}>Save</Button>
                 </div>
-            </div>
             </div>
         );
     }
